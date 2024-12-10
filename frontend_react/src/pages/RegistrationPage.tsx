d3193265423b05bc6a92868967b749bc710eedc2
Daniele Natola <dan.wding@gmail.com>
2024-12-10 17:39:05 +0100

    Create the registration form

---
+++ frontend_react/src/pages/RegistrationPage.tsx
@@ -0,0 +1,66 @@
+import { Button } from "@/components/ui/button";
+import { Field } from "@/components/ui/field";
+import { PasswordInput } from "@/components/ui/password-input";
+import { Input, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+interface RegistrationForm {
+    first_name: string;
+    last_name: string;
+    email: string;
+    password: string;
+    repeat_password: string;
+}
+
+const RegistrationPage = () => {
+    const [doPasswordsMatch, setPasswordMatch] = useState<boolean>(true);
+
+    const { register, handleSubmit } = useForm<RegistrationForm>();
+
+    const onSubmit = handleSubmit((data) => {
+        console.log("Inserted inputs: ", data);
+
+        if (data.password != data.repeat_password) {
+            setPasswordMatch(false);
+        } else {
+            // send the http-post-request containing user's data
+        }
+    });
+
+    return (
+        <form method="POST" onSubmit={onSubmit}>
+            <Field label="First name" required>
+                <Input placeholder="John" {...register("first_name")} />
+            </Field>
+
+            <Field label="Last name" required>
+                <Input placeholder="Doe" {...register("last_name")} />
+            </Field>
+
+            <Field label="Password" required>
+                <PasswordInput
+                    placeholder="**********"
+                    type="password"
+                    {...register("password")}
+                />
+            </Field>
+
+            <Field label="Repeat password" required>
+                <PasswordInput
+                    placeholder="**********"
+                    type="password"
+                    {...register("repeat_password")}
+                />
+            </Field>
+
+            {!doPasswordsMatch && (
+                <Text color={"red"}>Passwords must match</Text>
+            )}
+
+            <Button type="submit">Submit</Button>
+        </form>
+    );
+};
+
+export default RegistrationPage;
