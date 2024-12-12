16a1981a714a47234c97f27d6460cddd1d722d45
Daniele Natola <dan.wding@gmail.com>
2024-12-12 15:30:15 +0100

    Improve Navbar

--- frontend_react/src/components/Navbar.tsx
+++ frontend_react/src/components/Navbar.tsx
@@ -1,7 +1,11 @@
-import { HStack } from "@chakra-ui/react";
+import AuthContext from "@/contexts/authContext";
+import { Button, HStack } from "@chakra-ui/react";
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+    const { user, logout } = useContext(AuthContext);
+
     return (
         <HStack
             border={"2px solid black"}
@@ -10,8 +14,18 @@ const Navbar = () => {
             marginBottom={"20px"}
         >
             <NavLink to="/">Home</NavLink>
-            <NavLink to="/login">Login</NavLink>
-            <NavLink to="/registration">Register</NavLink>
+
+            {user ? (
+                <>
+                    <p>{user.username}</p>
+                    <Button onClick={logout}>Logout</Button>
+                </>
+            ) : (
+                <>
+                    <NavLink to="/login">Login</NavLink>
+                    <NavLink to="/registration">Register</NavLink>
+                </>
+            )}
         </HStack>
     );
 };
