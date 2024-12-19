9c90ea197599234a22aa6a5b780fbfbc3d4a87e3
Daniele Natola <dan.wding@gmail.com>
2024-12-19 23:51:11 +0100

    Implement FilterCampaignsByYear in the Admin App

--- backend_django/campaigns/admin.py
+++ backend_django/campaigns/admin.py
@@ -1,6 +1,31 @@
+from datetime import date
 from django.contrib import admin
+from django.http import HttpRequest
+from django.db.models import QuerySet
 from django.utils.html import format_html
 from .models import Campaign, Manual
+
+
+class FilterCampaignsByYear(admin.SimpleListFilter):
+    title = "Year"
+
+    def lookups(self, request: HttpRequest, model_admin: admin.ModelAdmin):
+        return [
+            # (query_string_value, human_readable_value)
+            (2018, "2018"),
+            (2019, "2019"),
+            (2020, "2020"),
+        ]
+
+    parameter_name = "release_year"
+
+    def queryset(self, request, queryset):
+        if self.value() == "2018":
+            return queryset.filter(release_date__year__exact=2018)
+        if self.value() == "2019":
+            return queryset.filter(release_date__year__exact=2019)
+        if self.value() == "2020":
+            return queryset.filter(release_date__year__exact=2020)
 
 
 # Register your models here.
@@ -16,7 +41,7 @@ class CampaignAdmin(admin.ModelAdmin):
         "slug",
     ]
     list_select_related = ["manual"]
-    list_filter = ["manual", "is_edited"]
+    list_filter = ["manual", "is_edited", FilterCampaignsByYear]
 
     @admin.display(ordering="name")
     def titlecase_name(self, campaign: Campaign):
