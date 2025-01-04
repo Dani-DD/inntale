e37b86b973733b8a291ee9414a5522060059411e
Daniele Natola <dan.wding@gmail.com>
2025-01-05 00:19:36 +0100

    Testing credits for players

--- frontend_react/src/components/Test.tsx
+++ frontend_react/src/components/Test.tsx
@@ -1,47 +1,14 @@
-import { Button, Stack } from "@chakra-ui/react";
-import FilteringButton from "./sidebar/FilteringButton";
-import { useState } from "react";
-import { IoIosArrowUp } from "react-icons/io";
-import { IoIosArrowDown } from "react-icons/io";
-
-const list = ["Dexter", "Debra", "Lundy", "Doakes", "Henry", "Rita", "Brian"];
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
 const Test = () => {
-    /**
-     * This component represents an expandable/collapsable list.
-     * The last element of the list is the button that set the list's status.
-     */
-
-    const [showAll, setShowAll] = useState<boolean>(false);
-
-    // Initial values: the list is collapsed by default.
-    let buttonText = "show all";
-    let buttonIcon = <IoIosArrowDown />;
-    let lastElement = 3;
-
-    if (showAll) {
-        buttonText = "hide";
-        buttonIcon = <IoIosArrowUp />;
-        lastElement = list.length;
-    }
-
     return (
-        <Stack>
-            {list.slice(0, lastElement).map((character) => (
-                <FilteringButton>{character}</FilteringButton>
-            ))}
-            <Button
-                width={"100px"}
-                size="md"
-                border="1px"
-                padding={"7px 15px"}
-                color={"black"}
-                leftIcon={buttonIcon}
-                onClick={() => setShowAll(!showAll)}
-            >
-                {buttonText}
-            </Button>
-        </Stack>
+        <Flex>
+            <Avatar src="https://bit.ly/sage-adebayo" />
+            <Box ml="3">
+                <Text fontWeight="bold">Andrea Guagnini</Text>
+                <Text fontSize="sm">as Rendar</Text>
+            </Box>
+        </Flex>
     );
 };
 
