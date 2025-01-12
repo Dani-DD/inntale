0e7beeedb17acb592d972e4634733e85ae948704
Daniele Natola <dan.wding@gmail.com>
2025-01-12 15:55:06 +0100

    Update NavbarLaptop

--- frontend_react/src/components/navbar/NavbarLaptop.tsx
+++ frontend_react/src/components/navbar/NavbarLaptop.tsx
@@ -2,8 +2,12 @@ import { HStack, Image } from "@chakra-ui/react";
 import logoLaptop from "../../assets/inntale_logo2.png";
 import { NavLink, Link } from "react-router-dom";
 import SearchBox from "./SearchBox";
+import { useContext } from "react";
+import AuthContext from "@/contexts/authContext";
 
 const NavbarLaptop = () => {
+    const { user, logout } = useContext(AuthContext);
+
     return (
         <HStack spacing="35px" padding="15px" justifyContent="space-between">
             <Link to="/">
@@ -15,15 +19,21 @@ const NavbarLaptop = () => {
                 />
             </Link>
             <SearchBox />
-            <HStack
-                height="100%"
-                spacing="35px"
-                padding="15px"
-                border="2px solid red"
-            >
-                <HStack spacing="10px" border="2px solid green">
-                    <NavLink to="/login">Login</NavLink>
-                    <NavLink to="/registration">Register</NavLink>
+            <HStack height="100%" spacing="35px" padding="15px">
+                <HStack spacing="10px">
+                    {user ? (
+                        <>
+                            <p>{user.username}</p>
+                            <NavLink to="/login" onClick={() => logout()}>
+                                Logout
+                            </NavLink>
+                        </>
+                    ) : (
+                        <>
+                            <NavLink to="/login">Login</NavLink>
+                            <NavLink to="/registration">Register</NavLink>
+                        </>
+                    )}
                 </HStack>
             </HStack>
         </HStack>
