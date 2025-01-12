650db9a1fea5393f7a88744d7f36e663f5e19263
Daniele Natola <dan.wding@gmail.com>
2025-01-12 14:41:33 +0100

    Refactor - Create the login form using the map method

--- frontend_react/src/pages/LoginPage.tsx
+++ frontend_react/src/pages/LoginPage.tsx
@@ -1,8 +1,16 @@
 import AuthContext from "@/contexts/authContext";
 import { UserCredentials } from "@/interfaces/allIntefaces";
-import { Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import { blue_inntale } from "@/utils/colors";
+import { Box, Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
 import { useContext } from "react";
 import { useForm } from "react-hook-form";
+
+// This is the structure of the TS object mapped to a Chakra UI's form
+interface ChakraLoginFormElement {
+    label: string;
+    type: string;
+    registerName: keyof UserCredentials;
+}
 
 const LoginPage = () => {
     const { register, handleSubmit } = useForm<UserCredentials>();
@@ -13,20 +21,44 @@ const LoginPage = () => {
         login(data);
     });
 
+    // This array will be used to contruct the form by calling the map method that will convert every object
+    // into a Chakra UI's FormControl element
+    const formStructure: ChakraLoginFormElement[] = [
+        {
+            label: "Username",
+            type: "text",
+            registerName: "username",
+        },
+        {
+            label: "Password",
+            type: "password",
+            registerName: "password",
+        },
+    ];
+
+    const chakraLoginForm = formStructure.map((formElement) => (
+        <FormControl isRequired>
+            <FormLabel>{formElement.label}</FormLabel>
+            <Input
+                type={formElement.type}
+                variant="filled"
+                marginBottom="20px"
+                {...register(formElement.registerName)}
+            />
+        </FormControl>
+    ));
+
     return (
-        <form method="POST" onSubmit={onSubmit}>
-            <FormControl isRequired>
-                <FormLabel>Username</FormLabel>
-                <Input {...register("username")} />
-            </FormControl>
-
-            <FormControl isRequired>
-                <FormLabel>Password</FormLabel>
-                <Input type="password" {...register("password")} />
-            </FormControl>
-
-            <Button type="submit">Log In</Button>
-        </form>
+        <Box backgroundColor={blue_inntale} paddingTop={"25px"}>
+            <Box maxWidth={"500px"} marginLeft="auto" marginRight="auto">
+                <form method="POST" onSubmit={onSubmit}>
+                    {chakraLoginForm}
+                    <Button type="submit" marginTop="20px" marginBottom="80px">
+                        Log In
+                    </Button>
+                </form>
+            </Box>
+        </Box>
     );
 };
 
