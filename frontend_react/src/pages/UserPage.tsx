0bd09320c0c150cd55d391c7ae68e7be0e73d446
Daniele Natola <dan.wding@gmail.com>
2025-01-16 11:12:09 +0100

    Show the user's watchlist in its personal page

--- frontend_react/src/pages/UserPage.tsx
+++ frontend_react/src/pages/UserPage.tsx
@@ -1,27 +1,44 @@
 import AuthContext from "@/contexts/authContext";
 import useWatchlist from "@/hooks/useWatchlist";
-import { ListItem, UnorderedList } from "@chakra-ui/react";
+import { blue_inntale } from "@/utils/colors";
+import {
+    Box,
+    Button,
+    Heading,
+    ListItem,
+    UnorderedList,
+} from "@chakra-ui/react";
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 
 const UserPage = () => {
     const { user } = useContext(AuthContext);
     const { watchlist } = useWatchlist();
-    console.log(watchlist);
 
     if (!user) {
         <Navigate to="/login" />;
     }
 
     return (
-        <>
+        <Box backgroundColor={blue_inntale} padding="20px">
+            <Heading>User's informations</Heading>
             <UnorderedList>
                 <ListItem>{user!.first_name}</ListItem>
                 <ListItem>{user!.last_name}</ListItem>
                 <ListItem>{user!.email}</ListItem>
                 <ListItem>{user!.username}</ListItem>
             </UnorderedList>
-        </>
+
+            <Heading>User's watchlist</Heading>
+            <UnorderedList>
+                {watchlist.map((watchlistItem) => (
+                    <>
+                        <ListItem>{`${watchlistItem.campaign.name} S${watchlistItem.campaign.season}`}</ListItem>
+                        <Button colorScheme="red">Remove from watchlist</Button>
+                    </>
+                ))}
+            </UnorderedList>
+        </Box>
     );
 };
 
