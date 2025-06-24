cf814a2ce994322d7f915afdeef6ecabebd26232
Daniele Natola <dan.wding@gmail.com>
2025-06-24 16:24:15 +0200

    FRONTEND (bugfix): fix the bug that fetches watchlist for anonymous users

--- frontend_react/src/components/main/LoadWatchlist.tsx
+++ frontend_react/src/components/main/LoadWatchlist.tsx
@@ -1,20 +1,23 @@
-// LoadWatchlist.tsx
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
+import AuthContext from "@/contexts/authContext";
 import useWatchlistStore from "@/stores/WatchlistStore";
 import usePrivateAxios from "@/hooks/usePrivateAxios";
 
 const LoadWatchlist = () => {
+    const { user } = useContext(AuthContext);
     const setWatchlist = useWatchlistStore((s) => s.setWatchlist);
     const axiosInstance = usePrivateAxios();
 
     useEffect(() => {
+        if (!user) return; // Skip fetch if anonymous
+
         axiosInstance
             .get("root/watchlist/")
             .then((response) => setWatchlist(response.data))
             .catch((error) => console.error("Watchlist fetch error:", error));
-    }, [axiosInstance, setWatchlist]);
+    }, [axiosInstance, setWatchlist, user]);
 
-    return null; // It doesn't render anything
+    return null;
 };
 
 export default LoadWatchlist;
