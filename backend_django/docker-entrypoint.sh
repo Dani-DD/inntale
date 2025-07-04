45d217fe3b26ccc737cad2271bdd2b95ca13ba78
Daniele Natola <dan.wding@gmail.com>
2025-07-04 13:24:33 +0200

    BACKEND (feature): collect static files

--- backend_django/docker-entrypoint.sh
+++ backend_django/docker-entrypoint.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
 # Apply database migrations
-echo "Apply database migrations"
+echo "Applying database migrations..."
 python manage.py migrate
 
-# Start server
-echo "Starting server"
-python manage.py runserver 0.0.0.0:8000
+# Check environment and start the correct server
+if [ "$ENVIRONMENT" = "development" ]; then
+    echo "Running in development mode..."
+    python manage.py runserver 0.0.0.0:8000
+else
+    echo "Collecting static files..."
+    python manage.py collectstatic --noinput
+    
+    echo "Running in production mode with Gunicorn..."
+    gunicorn inntale.wsgi:application \
+        --bind 0.0.0.0:8000 \
+        --timeout 120 \
+        --workers 3 \
+        --log-level debug \
+        --access-logfile - \
+        --error-logfile -
+
+fi
