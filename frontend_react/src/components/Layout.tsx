4457a58c2ae0cf54fb76f8038aa62466309f006b
Daniele Natola <dan.wding@gmail.com>
2025-07-06 00:49:06 +0200

    FRONTEND (feature): add some styles

--- frontend_react/src/components/Layout.tsx
+++ frontend_react/src/components/Layout.tsx
@@ -1,14 +1,21 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./navbar/Navbar";
 import LoadWatchlist from "./main/LoadWatchlist";
+import { Box } from "@chakra-ui/react";
+import { blue_inntale } from "@/utils/colors";
 
 const Layout = () => {
     return (
-        <>
+        <Box
+            backgroundColor={blue_inntale}
+            display="flex"
+            flexDirection={"column"}
+            minHeight={"100vh"}
+        >
             <Navbar />
             <LoadWatchlist />
             <Outlet />
-        </>
+        </Box>
     );
 };
 
